5d4cf1db2f1439b9272ccae865235acaf6915763
shubhamprshr27 <shubhamprshr27@gmail.com>
2022-01-01 02:32:21 +0530

    Initial Commit for practice

---
+++ practice/2 Sum/TwoSum.java
@@ -0,0 +1,12 @@
+
+public class TwoSum
+{
+
+    void printName(){
+        System.out.println("Hello World");
+    }
+    public static void main (String args[]){
+        TwoSum objSum = new TwoSum();
+        objSum.printName();
+    }
+}
